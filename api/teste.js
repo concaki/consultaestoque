ceb4cf7fcae3c71a814bfa7dc87262e262173840
concaki <48227300+concaki@users.noreply.github.com>
2025-06-29 14:46:55 -0300

    Update teste.js

--- api/teste.js
+++ api/teste.js
@@ -1,16 +1,3 @@
-import { Redis } from '@upstash/redis';
-
-const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN,
-});
-
 export default async function handler(req, res) {
-  try {
-    await redis.set("chaveTeste", "valorOK");
-    const resultado = await redis.get("chaveTeste");
-    res.status(200).json({ success: true, resultado });
-  } catch (e) {
-    res.status(500).json({ success: false, erro: e.message });
-  }
+  res.status(200).json({ success: true, message: 'Função testeFetch funcionando!' });
 }
