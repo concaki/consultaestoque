2a04b7fcb222e110bfce9b3cd5e0ed7f6175c928
concaki <48227300+concaki@users.noreply.github.com>
2025-06-29 14:44:13 -0300

    Update testeFetch.js

--- api/testeFetch.js
+++ api/testeFetch.js
@@ -1,14 +1,3 @@
 export default async function handler(req, res) {
-  try {
-    const response = await fetch("https://polite-fowl-10598.upstash.io/get/teste", {
-      headers: {
-        Authorization: "Bearer upstash:ASlmAAIjcDE5YTcyMWU3ZDMwNDE0OTVhYmEwMDJiZjdhNTI4OGZhN3AxMA"
-      }
-    });
-
-    const json = await response.json();
-    res.status(200).json({ success: true, result: json });
-  } catch (error) {
-    res.status(500).json({ success: false, error: error.message });
-  }
+  res.status(200).json({ success: true, message: 'Função testeFetch funcionando!' });
 }
