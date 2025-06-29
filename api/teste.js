4e6788f8d3cf480c6dbbe541d31201028cf5b6f3
concaki <48227300+concaki@users.noreply.github.com>
2025-06-29 14:13:34 -0300

    Update teste.js

--- api/teste.js
+++ api/teste.js
@@ -1,12 +1,16 @@
 import { Redis } from '@upstash/redis';
 
-const redis = Redis.fromEnv();
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+});
 
 export default async function handler(req, res) {
   try {
-    const token = await redis.get('bling_access');
-    res.status(200).json({ bling_access: token });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
+    await redis.set("teste", "funcionando");
+    const result = await redis.get("teste");
+    res.status(200).json({ result });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
   }
 }
